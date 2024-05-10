c54af61822d7eb79827f064aaba18ce694b97383
Benedek Ágota <agotabenjoebenedek@gmail.com>
2024-05-10 20:08:31 +0200

    enter and exit implemented

--- contracts/LaunchCodes.sol
+++ contracts/LaunchCodes.sol
@@ -9,10 +9,6 @@ contract Facility {
     string[] public logs;
     bool public isChangingGuard;
 
-    mapping(address => bool) public membersWaitingToEnter;
-    mapping(address => mapping(address => bool)) public firstGuardApprovedEnter;
-    mapping(address => mapping(address => bool)) public secondGuardApprovedEnter;
-
     struct Request {
         address member;
         bool isEnter;
@@ -22,9 +18,14 @@ contract Facility {
 
     mapping(address => Request) public requests;
 
+     constructor(address _firstGuard, address _secondGuard) {
+        firstGuard = _firstGuard;
+        secondGuard = _secondGuard;
+    }
+
     modifier approved() {
         require(
-            firstGuardApprovedEnter[msg.sender][msg.sender] && secondGuardApprovedEnter[msg.sender][msg.sender],
+            requests[msg.sender].firstGuardApproved && requests[msg.sender].secondGuardApproved,
             "Both guards must approve"
         );
         _;
@@ -35,96 +36,65 @@ contract Facility {
         _;
     }
 
-    constructor(address _firstGuard, address _secondGuard) {
-        firstGuard = _firstGuard;
-        secondGuard = _secondGuard;
-    }
-
     function requestEnter() external {
-        require(!isChangingGuard, "Guard changing is in progress");
-        require(membersInFacility.length < 3, "Facility is full");
-        require(!membersWaitingToEnter[msg.sender], "Already waiting to enter");
-        require(!requests[msg.sender].isEnter, "Already requested to enter");
-
-        membersWaitingToEnter[msg.sender] = true;
         requests[msg.sender] = Request(msg.sender, true, false, false);
     }
 
     function approveEnter(address member) external onlyGuard {
-        require(membersWaitingToEnter[member], "Member not waiting to enter");
-        require(!firstGuardApprovedEnter[member][msg.sender], "Already approved by first guard");
-        require(!secondGuardApprovedEnter[member][msg.sender], "Already approved by second guard");
+        require(requests[member].isEnter, "Member not waiting to enter");
+        require(membersInFacility.length == 3, "Facility is full");
 
         if (msg.sender == firstGuard) {
-            firstGuardApprovedEnter[member][msg.sender] = true;
+           requests[member].firstGuardApproved = true;
         } else {
-            secondGuardApprovedEnter[member][msg.sender] = true;
-        }
-
-        if (firstGuardApprovedEnter[member][msg.sender] && secondGuardApprovedEnter[member][msg.sender]) {
-            _doEnter(member);
+            requests[member].secondGuardApproved = true;
         }
     }
 
-    function _doEnter(address member) private approved() {
+    function doEnter(address member) external approved {
         isDoorOpen = true;
         membersInFacility.push(member);
-        logs.push(string(abi.encodePacked("Entered: ", member)));
-        delete membersWaitingToEnter[member];
+        isDoorOpen = false;
         delete requests[member];
+        logs.push(string(abi.encodePacked("Entered: ", member)));
     }
 
     function requestExit() external {
-        require(isDoorOpen, "Door is closed");
-        require(!isChangingGuard, "Guard changing is in progress");
-        require(membersInFacility.length > 0, "No members in facility");
-        require(membersInFacility[membersInFacility.length - 1] == msg.sender, "Only last member can exit");
-        require(!requests[msg.sender].isEnter, "Already requested to enter");
-
         requests[msg.sender] = Request(msg.sender, false, false, false);
     }
 
-    function approveExit() external {
-        require(msg.sender == firstGuard || msg.sender == secondGuard, "Only guards can approve");
-        require(requests[msg.sender].member == msg.sender, "Member not waiting to exit");
-        require(!firstGuardApprovedEnter[msg.sender][msg.sender], "Already approved by first guard");
-        require(!secondGuardApprovedEnter[msg.sender][msg.sender], "Already approved by second guard");
+    function approveExit(address member) external onlyGuard {
+        require(!requests[member].isEnter, "Member not waiting to exit");
+        require(member != firstGuard && member != secondGuard, "Guards on duty cannot exit");
 
         if (msg.sender == firstGuard) {
-            firstGuardApprovedEnter[msg.sender][msg.sender] = true;
+           requests[member].firstGuardApproved = true;
         } else {
-            secondGuardApprovedEnter[msg.sender][msg.sender] = true;
-        }
-
-        if (firstGuardApprovedEnter[msg.sender][msg.sender] && secondGuardApprovedEnter[msg.sender][msg.sender]) {
-            _doExit(msg.sender);
+            requests[member].secondGuardApproved = true;
         }
     }
 
-    function _doExit(address member) private approved() {
+    function doExit(address member) external approved {
+        isDoorOpen = true;
+        removeMember(member);
         isDoorOpen = false;
-        for (uint256 i = 0; i < membersInFacility.length; i++) {
-            if (membersInFacility[i] == member) {
-                membersInFacility[i] = address(0);
-                break;
-            }
-        }
-        logs.push(string(abi.encodePacked("Exited: ", member)));
         delete requests[member];
+        logs.push(string(abi.encodePacked("Exited: ", member)));
     }
 
-    function changeGuard(address newGuard, bool isFirstGuard) external {
-        require(!isChangingGuard, "Guard changing is in progress");
-        isChangingGuard = true;
-
-        if (isFirstGuard) {
-            firstGuard = newGuard;
-        } else {
-            secondGuard = newGuard;
+    //segédfüggvény, a solidity nem tud simán törölni tetszőleges egy elemet egy tömbből
+    function removeMember(address member) public {
+        uint index = 0;
+        while (index < membersInFacility.length && membersInFacility[index] != member) {
+            index++;
         }
 
-        isChangingGuard = false;
+        if (index < membersInFacility.length) {
+            // Move the last element into the place of the one to delete
+            membersInFacility[index] = membersInFacility[membersInFacility.length-1];
+            // Remove the last element
+            membersInFacility.pop();
+        }
     }
-
-
+   
 }
